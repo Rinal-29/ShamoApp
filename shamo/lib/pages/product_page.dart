22935b79dc340f9347a7f44930ad988d6948b8b0
Rinal-29 <afrinalhakim29@gmail.com>
2021-11-09 19:17:39 +0800

    finish product page slice

--- shamo/lib/pages/product_page.dart
+++ shamo/lib/pages/product_page.dart
@@ -30,9 +30,84 @@ class _ProductPageState extends State<ProductPage> {
   ];
 
   int currentIndex = 0;
+  bool isWhislist = false;
 
   @override
   Widget build(BuildContext context) {
+    Future<void> showSuccessDialog() async {
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) => Container(
+          width: MediaQuery.of(context).size.width - (defaultMargin * 2),
+          child: AlertDialog(
+            backgroundColor: bgColor3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.close,
+                        color: primaryTextColor,
+                      ),
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/Icon.png',
+                    width: 100,
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Hurray :)',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'item added successfuly',
+                    style: secondaryTextStyle,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 44,
+                    width: 154,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                          backgroundColor: primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          )),
+                      child: Text(
+                        'View My Cart',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
     Widget indicator(int index) {
       return Container(
         width: currentIndex == index ? 16 : 4,
@@ -163,9 +238,30 @@ class _ProductPageState extends State<ProductPage> {
                       ),
                     ],
                   ),
-                  Image.asset(
-                    'assets/button_wishist.png',
-                    width: 46,
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isWhislist = !isWhislist;
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor:
+                              isWhislist ? secondaryColor : alertColor,
+                          content: Text(
+                            isWhislist
+                                ? 'Berhasil menambah wishlist'
+                                : 'Berhasil menghapus wislisht',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Image.asset(
+                      isWhislist
+                          ? 'assets/success_icon.png'
+                          : 'assets/button_wishist.png',
+                      width: 46,
+                    ),
                   ),
                 ],
               ),
@@ -272,12 +368,17 @@ class _ProductPageState extends State<ProductPage> {
                     margin: EdgeInsets.all(defaultMargin),
                     child: Row(
                       children: [
-                        Container(
-                          width: 54,
-                          height: 54,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/button_chat.png'),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/detail-chat');
+                          },
+                          child: Container(
+                            width: 54,
+                            height: 54,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/button_chat.png'),
+                              ),
                             ),
                           ),
                         ),
@@ -286,7 +387,9 @@ class _ProductPageState extends State<ProductPage> {
                           child: Container(
                             height: 54,
                             child: TextButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                showSuccessDialog();
+                              },
                               style: TextButton.styleFrom(
                                 backgroundColor: primaryColor,
                                 shape: RoundedRectangleBorder(
